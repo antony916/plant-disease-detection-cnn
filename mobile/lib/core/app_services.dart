daff8596cd5bfac71955cfc24d8630d2b5a930ea
Antony Paul Renio <antony@csice.edu.in>
2026-09-25 14:36:18 +0530

    Connect the diagnosis service to the mobile service container

--- mobile/lib/core/app_services.dart
+++ mobile/lib/core/app_services.dart
@@ -1,9 +1,11 @@
 import 'services/demo_services.dart';
+import 'services/diagnosis_service.dart';
 
 class AppServices {
   const AppServices._();
 
   static final auth = DemoAuthService();
   static final garden = DemoGardenRepository();
+  static final diagnosis = DemoDiagnosisService();
   static final notifications = DemoNotificationService();
 }
