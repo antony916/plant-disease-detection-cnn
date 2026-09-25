14202a5b151703eafcaa8dd36f3034a50cd04ae7
Antony Paul Renio <antony@csice.edu.in>
2026-09-25 16:17:11 +0530

    Align cloud auth with app auth contract

--- mobile/lib/core/services/cloud_auth_service.dart
+++ mobile/lib/core/services/cloud_auth_service.dart
@@ -1,10 +1,3 @@
 import 'auth_service.dart';
 
-abstract interface class CloudAuthService {
-  Future<AppUser?> currentUser();
-  Future<AppUser> signInWithEmail({
-    required String email,
-    required String password,
-  });
-  Future<void> signOut();
-}
+abstract class CloudAuthService extends AuthService {}
