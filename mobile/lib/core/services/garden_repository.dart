5ca69051fa4028c563a92cead41dedeef8a4b881
Antony Paul Renio <antony@csice.edu.in>
2026-09-25 16:21:20 +0530

    Add garden loading to repository contract

--- mobile/lib/core/services/garden_repository.dart
+++ mobile/lib/core/services/garden_repository.dart
@@ -1,7 +1,9 @@
+import '../models/garden.dart';
 import '../models/plant.dart';
 import '../models/garden_task.dart';
 
 abstract interface class GardenRepository {
+  Future<Garden> getGarden();
   Future<List<Plant>> getPlants();
   Future<Plant> addPlant(Plant plant);
   Future<void> updatePlant(Plant plant);
