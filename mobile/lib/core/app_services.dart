a08574d6e2a680c80df393c9ec117c4e110632cd
Antony Paul Renio <antony@csice.edu.in>
2026-09-25 14:55:31 +0530

    Wire real weather and optional location services

--- mobile/lib/core/app_services.dart
+++ mobile/lib/core/app_services.dart
@@ -1,6 +1,7 @@
 import 'services/care_service.dart';
 import 'services/demo_services.dart';
 import 'services/diagnosis_service.dart';
+import 'services/location_service.dart';
 import 'services/weather_service.dart';
 
 class AppServices {
@@ -10,6 +11,9 @@ class AppServices {
   static final garden = DemoGardenRepository();
   static final diagnosis = DemoDiagnosisRepository(DemoDiagnosisService());
   static final notifications = DemoNotificationService();
-  static final weather = DemoWeatherService();
+  static final location = SelectedLocationService();
+  static final weather = OpenMeteoWeatherService(
+    locationService: location,
+  );
   static final care = RuleBasedCareService(weather);
 }
