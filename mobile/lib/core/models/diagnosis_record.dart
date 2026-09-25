ca4588d92d4b3ee58796c47d1eeebeca01fc8989
Antony Paul Renio <antony@csice.edu.in>
2026-09-25 16:31:28 +0530

    Separate diagnosis image path from stored reference

--- mobile/lib/core/models/diagnosis_record.dart
+++ mobile/lib/core/models/diagnosis_record.dart
@@ -4,6 +4,7 @@ class DiagnosisRecord {
   final String id;
   final String plantId;
   final String imagePath;
+  final String? imageReference;
   final DiagnosisResult result;
   final DateTime createdAt;
 
@@ -11,6 +12,7 @@ class DiagnosisRecord {
     required this.id,
     required this.plantId,
     required this.imagePath,
+    this.imageReference,
     required this.result,
     required this.createdAt,
   });
